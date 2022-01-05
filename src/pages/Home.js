e2dcd9c5bd5cf32677708ba5e66ab17ed0e9f10e
cathngo <cngo1080@gmail.com>
2022-01-05 18:48:26 +1100

    Finished basic layout of home page (html and css)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default function Home() {
     return (
-        <></>
+        <>
+            <div className='home-container'>
+                <article>
+                    <p className='home-header'>Word of the Day</p>
+                    <div className='home-underline'></div>
+                    <div className='daily-word-box'>
+                        <div className='word-noun'>
+                            <h2 className='title'>kapta</h2>
+                            <p >noun</p>
+                        </div>
+                        <p className='daily-word-definition'>A shirt of reindeer-skin worn by the Laplandas</p>
+                    </div>
+                </article>
+            </div>
+        </>
     )
 }

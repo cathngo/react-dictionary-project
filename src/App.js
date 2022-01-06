43cfd6911c53b587ed94c6ffa417a1337d212875
cathngo <cngo1080@gmail.com>
2022-01-06 13:59:34 +1100

    Added navigation from react router

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import Home from './pages/Home'
 import Navbar from './components/Navbar';
 import SavedWords from './pages/SavedWords'
 import Dictionary from './pages/Dictionary';
+//router
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 function App() {
   return (
-    <>
+
+    <Router>
       <Navbar/>
-      <Dictionary/>
-    </>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/save' element={<SavedWords/>}/>
+        <Route path='/define/:word' element={<Dictionary/>}/>
+      </Routes>
+    </Router>
+
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,17 +2,23 @@ import React from 'react'
 import logo from '../logo.svg'
 import bookmark from '../bookmark.svg'
 import search from '../search.svg'
-
+//link
+import { Link } from "react-router-dom";
 export default function Navbar() {
+    
     return (
         <nav className='nav-header'>
             <div className='nav-left'>
+              
                 <img src={logo} alt='logo' className='logo'/>
+                
                 <ul>
-                    <li className='dict-text'>Dictionary</li>
+                    <li className='dict-text'>
+                          <Link to='/' className='link'>Dictionary</Link>
+                    </li>
                 </ul>
+    
             </div>
-       
             <form className='search'>
                 <div className='language'>English</div>
                 <input type='text' placeholder='Search for a word' className='search-bar'/>
@@ -22,10 +28,11 @@ export default function Navbar() {
             <div className='nav-right'>
                 <img src={bookmark} alt='bookmark' className='bookmark'/>
                 <ul>
-                    <li>Saved Words</li>
+                    <li>
+                    <Link to='/save' className='link'>Saved Words</Link>
+                    </li>
                 </ul>
             </div>
-
         </nav>
     )
 }

ece2f57ee1de23c0f08e45e7437584ab4b478479
cathngo <cngo1080@gmail.com>
2022-01-08 22:43:11 +1100

    Add onClick method for saved words to see definition

--- src/pages/SavedWords.js
+++ src/pages/SavedWords.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import history from '../history';
 
 export default function SavedWords() {
     
@@ -9,6 +10,13 @@ export default function SavedWords() {
         displayList = storedWords
     }
 
+    //display definition of synonym clicked
+    const handleWord = (word) => {
+        history.push(`/define/${word}`)
+        window.location.reload()
+    }
+
+
     return (
         <>
             <div className='save-container'>
@@ -16,7 +24,7 @@ export default function SavedWords() {
                 <div className='vocab-underline'></div>
                 <div className='saved-words'>
                     {displayList.map((item, index)=> {
-                        return <p key={index+23}>{item}</p>
+                        return <p key={index+23} onClick={()=>{handleWord(item)}}>{item}</p>
                     })}
                 </div>
 

1eaa7aafbd549761f7aa0cc47e01880dd5d65f17
cathngo <cngo1080@gmail.com>
2022-01-09 23:05:28 +1100

    Cleaned save and remove word logic

--- src/pages/SavedWords.js
+++ src/pages/SavedWords.js
@@ -3,17 +3,8 @@ import history from '../history';
 import trash from '../trash.svg'
 
 export default function SavedWords() {
-    /*
     const storedWords = JSON.parse(localStorage.getItem('savedWords'));
-
-    var displayList = ['no words']
-    if (storedWords !== null) {
-        displayList = storedWords
-    }
-    */
-    
-    const storedWords = JSON.parse(localStorage.getItem('savedWords'));
-    const [displayList, setDisplayList] = useState(storedWords === null? ['no words']: storedWords)
+    const [displayList, setDisplayList] = useState(storedWords === null? ['emptyList']: storedWords)
 
     //display definition of synonym clicked
     const handleWord = (word) => {
@@ -23,26 +14,20 @@ export default function SavedWords() {
 
     //delete item from list 
     const handleDelete = (word) => {
-        /*
-        localStorage.removeItem(word)
-        const newList = displayList.filter(removedWord => removedWord !== word)
-        setDisplayList(newList)
-        console.log('here')
-        */
-               for (let i = 0; i < storedWords.length; i++) {
+
+        for (let i = 0; i < storedWords.length; i++) {
             console.log(storedWords[i])
         }
-
-                //remove word
-        localStorage.removeItem(`${word}Stored`)
         
         //create new list without target word to remove
         const newList = storedWords.filter(item => item !== word)
         localStorage.setItem('savedWords', JSON.stringify(newList))
         setDisplayList(newList)
+       
+        if (newList.length < 1) {
+            setDisplayList(['emptyList'])
+        }
     }
-
-
 
     return (
         <>
@@ -50,10 +35,13 @@ export default function SavedWords() {
                 <p className='vocab-list-title'>Your Vocabulary List</p>
                 <div className='vocab-underline'></div>
                 <div className='saved-words'>
-                    {displayList.map((item, index)=> {
-                        return (
+                    {
+                        
+                    displayList.map((item, index)=> {
+           
+                        return (item === 'emptyList'? <p>Your list is empty. Search for a word and click on the bookmark icon to save it!</p>:
                         <div className='word-delete'>
-                            <p key={index+23} onClick={()=>{handleWord(item)}}>{item}</p>
+                            <p key={index+23} onClick={()=>{handleWord(item)}} className='click-word'>{item}</p>
                             <img src={trash} alt='delete' className='delete' onClick={()=>handleDelete(item)}/>
                         </div>)
                     })}
